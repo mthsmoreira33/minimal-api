01411b0467520535149d8edbcf7869ae9e4bafa1
Matheus Moreira <mthsmoreira59@gmail..com>
2025-01-09 14:33:59 -0300

    Initialize properties in LoginDTO with default values

--- Dominio/DTOs/LoginDTO.cs
+++ Dominio/DTOs/LoginDTO.cs
@@ -2,7 +2,7 @@ namespace minimal_api.Dominio.DTOs
 {
     public class LoginDTO
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
     }
 }
