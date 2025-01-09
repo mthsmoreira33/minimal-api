f44975ea286dc021297378f67975a0fc69b578d3
Matheus Moreira <mthsmoreira59@gmail..com>
2025-01-09 14:25:40 -0300

    Add Administrador class for managing administrator entities

---
+++ Dominio/Entidades/Administrador.cs
@@ -0,0 +1,7 @@
+namespace minimal_api.Dominio.Entidades {
+    public class Administrador {
+        public int Id { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Senha { get; set; } = default!;
+    }
+}
