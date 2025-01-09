5441c386297e51e68a8feb960e4872b5185ac6be
Matheus Moreira <mthsmoreira59@gmail..com>
2025-01-09 14:35:15 -0300

    Add Perfil property to Administrador class

--- Dominio/Entidades/Administrador.cs
+++ Dominio/Entidades/Administrador.cs
@@ -3,5 +3,6 @@ namespace minimal_api.Dominio.Entidades {
         public int Id { get; set; } = default!;
         public string Email { get; set; } = default!;
         public string Senha { get; set; } = default!;
+        public string Perfil { get; set; } = default!;
     }
 }
