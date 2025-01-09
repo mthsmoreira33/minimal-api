8dd295d560181ac57567f25f88e475e90673e86d
Matheus Moreira <mthsmoreira59@gmail..com>
2025-01-09 14:48:24 -0300

    Add properties and DbSet for Administrador entity in the context

--- Dominio/Entidades/Administrador.cs
+++ Dominio/Entidades/Administrador.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace minimal_api.Dominio.Entidades {
     public class Administrador {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; } = default!;
+        [Required]
+        [StringLength(255)]
         public string Email { get; set; } = default!;
+        [StringLength(100)]
         public string Senha { get; set; } = default!;
+        [StringLength(10)]
         public string Perfil { get; set; } = default!;
     }
 }
--- Infraestrutura/Db/AppDbContext.cs
+++ Infraestrutura/Db/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Entidades;
 
 namespace minimal_api.Infraestrutura.Db
 {
@@ -7,6 +8,12 @@ namespace minimal_api.Infraestrutura.Db
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-        
+
+        public DbSet<Administrador> Administradores { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
     }
 }
