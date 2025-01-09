3e0777d7176519e1d85d25888ea78357aabcfb9b
Matheus Moreira <mthsmoreira59@gmail..com>
2025-01-09 14:54:30 -0300

    Refactor AppDbContext to accept IConfiguration for improved configuration management

--- Infraestrutura/Db/AppDbContext.cs
+++ Infraestrutura/Db/AppDbContext.cs
@@ -5,8 +5,11 @@ namespace minimal_api.Infraestrutura.Db
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        private readonly IConfiguration _configurationAppSettings;
+        
+        public AppDbContext(IConfiguration configurationAppSettings)
         {
+            _configurationAppSettings = configurationAppSettings;
         }
 
         public DbSet<Administrador> Administradores { get; set; } = default!;
