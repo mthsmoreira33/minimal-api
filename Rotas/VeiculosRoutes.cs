a67fbee3b2e4e1c55b4ee8b5fd4e97150e8031e9
Matheus Moreira <mthsmoreira59@gmail..com>
2025-01-10 17:02:08 -0300

    Add new routes for vehicle operations in VeiculosRoutes.cs

--- Rotas/VeiculosRoutes.cs
+++ Rotas/VeiculosRoutes.cs
@@ -11,23 +11,45 @@ namespace minimal_api.Rotas
         {
             var veiculosRoutes = app.MapGroup("/veiculos");
 
+            // Get de todos os veículos
             veiculosRoutes.MapGet("/", ([FromQuery] int pagina, [FromQuery] string? nome, [FromQuery] string? marca, IVeiculoService veiculoService) =>
             {
                 return Results.Ok(veiculoService.Todos(pagina, nome, marca));
             }).WithTags("Veiculos");
 
+            // Get de todos os veículos no Id
             veiculosRoutes.MapGet("/{id}", ([FromRoute] int id, IVeiculoService veiculoService) =>
             {
-                return Results.Ok(veiculoService.BuscaPorId(id));
+                var veiculo = veiculoService.BuscaPorId(id);
+                if (veiculo == null) return Results.NotFound();
+                return Results.Ok(veiculo);
             }).WithTags("Veiculos");
 
-
-
+            // Post de veículos
             veiculosRoutes.MapPost("/", ([FromBody] AddVeiculoDTO veiculoDTO, IVeiculoService veiculoService) =>
             {
                 var veiculo = new Veiculo { Nome = veiculoDTO.Nome, Marca = veiculoDTO.Marca};
                 veiculoService.Incluir(veiculo);
                 return Results.Created();
+            }).WithTags("Veiculos");
+
+            // Put de veículos
+            veiculosRoutes.MapPut("/{id}", ([FromRoute] int id, [FromBody] AddVeiculoDTO veiculoDTO, IVeiculoService veiculoService) =>
+            {
+                var veiculo = veiculoService.BuscaPorId(id);
+                if (veiculo == null) return Results.NotFound();
+                veiculo.Nome = veiculoDTO.Nome;
+                veiculo.Marca = veiculoDTO.Marca;
+                veiculoService.Atualizar(veiculo);
+                return Results.Ok();
+            }).WithTags("Veiculos");
+
+            veiculosRoutes.MapDelete("/{id}", ([FromRoute] int id, IVeiculoService veiculoService) =>
+            {
+                var veiculo = veiculoService.BuscaPorId(id);
+                if (veiculo == null) return Results.NotFound();
+                veiculoService.Apagar(veiculo);
+                return Results.NoContent();
             }).WithTags("Veiculos");
         }
     }
