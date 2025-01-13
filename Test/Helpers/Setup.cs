422195ca57db280d322d3924573d7f9422b13450
Matheus Moreira <mthsmoreira59@gmail..com>
2025-01-13 17:15:30 -0300

    Add integration test setup and mock admin service

---
+++ Test/Helpers/Setup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using minimal_api.Domain.Interfaces;
+using Test.Mocks;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Setup
+{
+    public const string PORT = "5001";
+    public static TestContext testContext = default!;
+    public static WebApplicationFactory<Setup> http = default!;
+    public static HttpClient client = default!;
+
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.testContext = testContext;
+        Setup.http = new WebApplicationFactory<Setup>();
+
+        Setup.http = Setup.http.WithWebHostBuilder(builder =>
+        {
+            builder.UseSetting("https_port", Setup.PORT).UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped<IAdminService, AdminServiceMock>();
+            });
+
+        });
+
+        Setup.client = Setup.http.CreateClient();
+    }
+
+    public static void ClassCleanup()
+    {
+        Setup.http.Dispose();
+    }
+}
---
+++ Test/Mocks/AdminServiceMock.cs
@@ -0,0 +1,48 @@
+﻿using minimal_api.Domain.DTOs;
+using minimal_api.Domain.Entities;
+using minimal_api.Domain.Interfaces;
+
+namespace Test.Mocks
+{
+    public class AdminServiceMock : IAdminService
+    {
+        public static readonly List<Admin> admins =
+        [
+            new Admin {
+                Id = 1,
+                Email = "adm@teste.com",
+                Password = "123456",
+                Role = "Adm"
+            },
+            new Admin {
+                Id = 2,
+                Email = "editor@teste.com",
+                Password = "123456",
+                Role = "Editor"
+            }
+        ];
+
+        public Admin? SearchById(int id)
+        {
+            return admins.Where(v => v.Id == id).FirstOrDefault();
+        }
+        public Admin Store(Admin admin)
+        {
+            admins.Add(admin);
+            return admin;
+        }
+        public bool HasAny(AdminDTO adminDTO)
+        {
+            return (admins.Where(a => a.Email == adminDTO.Email).Any());
+        }
+        public List<Admin> GetAll(int? page)
+        {
+            return admins;
+        }
+
+        public Admin? Login(LoginDTO loginDTO)
+        {
+            return (admins.Where(a => a.Email == loginDTO.Email && a.Password == loginDTO.Password).FirstOrDefault());
+        }
+    }
+}
